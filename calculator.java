71c8b6ad413f1d8a96f46a5d9b2e892d1aed8436
succhi111 <suchendramishra396@gmail.com>
2022-08-11 10:51:12 +0530

    2nd java program

---
+++ calculator.java
@@ -0,0 +1,11 @@
+public class calculator {
+    public static void main(String[] args) {
+        calculator calculator = new calculator();
+        calculator.add(10, 20);
+    }
+        public void add (int number1, int number2) {
+         int add = number1 + number2;
+            System.out.println("sum of two no. is: " +add);
+
+    }
+}
