cc159de5eb5e2e659d1c4482b422defe21200cf0
succhi111 <suchendramishra396@gmail.com>
2022-08-13 12:31:15 +0530

    my new java programs

---
+++ SwitchVowel.java
@@ -0,0 +1,27 @@
+
+public class SwitchVowel {
+
+    public static void main(String[] args) {
+        char ch='e';
+        switch(ch)
+        {
+            case 'a':
+                System.out.println("Vowel");
+                break;
+            case 'e':
+                System.out.println("Vowel");
+                break;
+            case 'i':
+                System.out.println("Vowel");
+                break;
+            case 'o':
+                System.out.println("Vowel");
+                break;
+            case 'u':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("Consonant");
+        }
+    }
+}
